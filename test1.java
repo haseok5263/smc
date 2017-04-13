147a3d15f94deaada21d753c1423663164b06d64
chs4820 <chs526323@gmail.com>
2017-04-13 14:37:27 +0900

    BC 2017-04-13

--- test1.java
+++ test1.java
@@ -1,6 +1,8 @@
 public class test1{
-public static void main(String args[]){
-System.out.println("Hello Java");
+	public static void main(String args[]){
+		System.out.println("Hello Java");
+		System.out.println("good bye BC");
+
 
 	}
 }
